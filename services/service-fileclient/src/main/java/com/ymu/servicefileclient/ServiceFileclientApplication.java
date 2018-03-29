ec83f9e661db26ecd7289cc20aa4266fd83578bb
xinxiamu <932852117@qq.com>
2018-03-29 22:45:56 +0800

    更改

---
+++ services/service-fileclient/src/main/java/com/ymu/servicefileclient/ServiceFileclientApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.servicefileclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFileclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFileclientApplication.class, args);
+	}
+}
